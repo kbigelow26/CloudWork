245cbcf6f9b84218c374e4a74d0b0c016fc62151
Kaylee <KayleeBigelow@gmail.com>
2020-09-18 14:24:33 -0400

    cd is working

--- awsS3Shell.py
+++ awsS3Shell.py
@@ -4,12 +4,56 @@ import uuid
 import boto3
 
 
-# session = boto3.Session(profile_name='kaylee')
-# dev_s3_client = session.client('s3')
+def cd(client, path, newlocation):
+    # get all buckets
+    originalPath = path
+    bucketArray = []
+    for bucket in client.buckets.all():
+        bucketArray.append(bucket.name)
+    if not path:
+        path = []
+    else:
+        path = path.split("/")
 
-def cd(client):
-    for my_bucket_object in my_bucket.objects.all():
-        print(my_bucket_object)
+    if newlocation:
+        for loc in newlocation.split("/"):
+            if loc == "~":
+                path = []
+            elif loc == "..":
+                if len(path) > 0:
+                    path.pop()
+            else:
+                if loc in bucketArray and len(path) == 0:
+                    path.append(loc)
+                elif loc not in bucketArray and len(path) == 0:
+                    print("Invalid Path - A")
+                    return originalPath
+                else:
+                    path.append(loc)
+    else:
+        print("Missing Arguements")
+        return originalPath
+
+    if len(path) > 0:
+        my_bucket = client.Bucket(path[0])
+        bucketName = path[0]
+        if len(path) == 1:
+            return bucketName
+        path = '/'.join(path)
+        path = path.replace(bucketName+"/", "", 1)
+        for my_bucket_object in my_bucket.objects.all():
+            print("path: " + path)
+            print("compare: "+my_bucket_object.key)
+            if my_bucket_object.key[-1:] == "/":
+                curr = my_bucket_object.key[:-1]
+            else:
+                curr = my_bucket_object.key
+            if path == curr:
+                return bucketName + "/" + path
+        print("Invalid Path - B")
+        return originalPath
+    else:
+        return None
 
 
 def cp(client, path, originalFile, newFile):
@@ -89,8 +133,11 @@ def ls(client):
         print(bucket.name)
 
 
-def pwd(client):
-    print("WIP")
+def pwd(path):
+    if path:
+        print("s3:/"+path)
+    else:
+        print("s3:/")
 
 
 def rmdir(client, path, folder):
@@ -175,12 +222,12 @@ def availableCommands():
     print("logout - terminates connection")
     print("ls <-l> - lists buckets or the objects in a bucket, <-l> is an optional parameter to print long form descrition")
     print("mkbucket <S3 bucket name> - makes a S3 bucket in AWS")
-    print("mkdir - makes a directory")
+    print("mkdir <directory> - makes a directory")
     print("mv <S3 object name> <S3 object name> - moves an object from one S3 location to another")
     print("pwd - displays position in the directory tree")
     print("quit - terminates connection")
     print("rm <S3 object name> - deletes object")
-    print("rmdir - removes directory")
+    print("rmdir <directory> - removes directory")
     print("upload <local filename> <S3 object name> - copies local file to S3 object store")
 
 
@@ -211,7 +258,10 @@ def main():
             if userInput[0] == "help":
                 availableCommands()
             elif userInput[0] == "cd":
-                cd(client)
+                if len(userInput) == 2:
+                    path = cd(resource, path, userInput[1])
+                else:
+                    print("Missing arguments")
             elif userInput[0] == "cp":
                 if len(userInput) == 3:
                     cp(resource, path, userInput[1], userInput[2])
@@ -249,7 +299,7 @@ def main():
                     print("Missing arguments")
             elif userInput[0] == "pwd":
                 if len(userInput) == 1:
-                    pwd(client)
+                    pwd(path)
                 else:
                     print("Too many arguments")
             elif userInput[0] == "rmdir":
